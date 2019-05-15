dc5c345c0de32c5978a1874afceaf9fbb97e1eab
Ryan Skrzypek <ryanskrzypek@gmail.com>
2019-05-15 16:35:25 -0500

    Added repo and repoitem components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,26 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script>
-const axios = require('axios')
-axios.get('https://swapi.co/api/people/1/')
-  .then(function(response){
-    console.log('Success!')
-    console.log(response)
-  })
-  .catch(function(error){
-    console.log('Failure!')
-    console.log(error)
-  })
 export default {
-  
+  name: 'app'
 }
 </script>
 
