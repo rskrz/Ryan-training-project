8d4ac7b1ad97e45b2a3a10f2e8c610c554e5392b
Ryan Skrzypek <rskrzypek@uiowa.edu>
2019-10-31 18:11:04 -0500

    added PurifyCSS to reduce CSS entry point chunk size

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,39 @@
-configureWebpack: {
-  module: {
+const path = require("path");
+
+const PurifyCSSPlugin = require("purifycss-webpack");
+const glob = require("glob-all");
+
+module.exports = {
+  configureWebpack: {
+    module: {
       rules: [
         {
           test: /\.pug$/,
-          loader: "pug-plain-loader",
+          loader: "pug-plain-loader"
         }
       ]
-  }
-}
-const path = require("path");
-
-module.exports = {
+    },
+    plugins: [
+      new PurifyCSSPlugin({
+        paths: glob.sync([
+          path.join(__dirname, "src/*.html"),
+          path.join(__dirname, "src/*.js")
+        ]),
+        minimize: true,
+        purifyOptions: {
+          whitelist: []
+        }
+      })
+    ]
+  },
   pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [
-        path.resolve(__dirname, './src/styles/_global.scss')
-      ]
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.resolve(__dirname, "./src/styles/_global.scss")]
     }
   },
   devServer: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     disableHostCheck: true
   }
 };
